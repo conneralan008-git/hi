f14ceb3aa7661b1eee524f56265c13519ab54b6e
Aditya Nagdeve <adnagdeve2810@gmail.com>
2024-01-28 21:50:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let showbutton = document.querySelector(".Rulesbutton");
+let rule = document.querySelector(".gamesrules");
+let btn = document.querySelector(".close");
+
+let isRuleBoxOpen = true;
+
+// Event listener for the close button
+btn.addEventListener("click", () => {
+  rule.style.display = "none";
+  isRuleBoxOpen = false;
+});
+
+// Event listener for the show rules button
+showbutton.addEventListener("click", () => {
+  rule.style.display = "block";
+  isRuleBoxOpen = true;
+});
+
